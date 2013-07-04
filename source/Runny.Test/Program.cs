aa33dee0d471b21e0e85bdf0f026676f1ade7e99
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2013-07-04 18:34:08 +0200

    introduced test project to hit self hosted service

---
+++ source/Runny.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using Agatha.Common;
+using Agatha.Common.InversionOfControl;
+using Agatha.StructureMap;
+using Runny.Commands;
+using System;
+
+namespace Runny.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ClientConfiguration
+                (
+                    requestsAndResponsesAssembly: typeof(EchoRequest).Assembly,
+                    containerImplementation: typeof(Container)
+                );
+            configuration.Initialize();
+
+            var dispatcher = IoC.Container.Resolve<IRequestDispatcher>();
+            var response = dispatcher.Get<EchoResponse>(new EchoRequest { Message = "Hi melkio" });
+            Console.WriteLine(response.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
