06989f19f4b47d4f3e72d828ffce68db96a9989d
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2013-07-05 15:42:13 +0200

    runny can run powershell init/install script

--- source/Runny/Handlers/InstallHandler.cs
+++ source/Runny/Handlers/InstallHandler.cs
@@ -15,15 +15,13 @@ namespace Runny.Handlers
             var packageManager = new PackageManager(source, request.Location);
 
             var sourcePackage = packageManager.SourceRepository.FindPackage(request.ApplicationName);
-            //Log.Debug(String.Format("Server version: {0}", sourcePackage.Version));
             var localPackage = packageManager.LocalRepository.FindPackage(request.ApplicationName);
-            //Log.Debug(String.Format("Local version: {0}", localPackage == null ? "--" : localPackage.Version.ToString()));
 
             if (localPackage == null || localPackage.Version < sourcePackage.Version)
             {
-                //Log.Info(String.Format("Installing version {0} for application : {1}", sourcePackage.Version, a.Name));
                 packageManager.InstallPackage(request.ApplicationName);
-                //RunScript(a);
+                localPackage.ExecuteInitIfExists(request.Location);
+                localPackage.ExecuteInstallIfExists(request.Location);
             }
 
             return response;
---
+++ source/Runny/PackageExtensionMethods.cs
@@ -0,0 +1,58 @@
+﻿using NuGet;
+using System;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace Runny
+{
+    static class PackageExtensionMethods
+    {
+        public static void ExecutePowershellScriptFileIfExists(this IPackage package, String scriptName, String locationRoot)
+        {
+            var script = package
+                .GetToolFiles()
+                .Where(f => Path.GetExtension(f.Path) == ".ps1")
+                .SingleOrDefault(f => Path.GetFileNameWithoutExtension(f.Path) == scriptName);
+
+            if (script == null) return;
+
+            var installPath = Path.Combine(locationRoot, String.Format("{0}.{1}", package.Id, package.Version));
+            var scriptPath = Path.Combine(installPath, script.Path);
+            var toolsPath = Path.GetDirectoryName(scriptPath);
+
+            var runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+
+            var setExecutionPolicy = new RunspaceInvoke();
+            setExecutionPolicy.Invoke("Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process");
+
+            var pipeline = runspace.CreatePipeline();
+            var command = new Command(scriptPath);
+            command.Parameters.Add("installPath", installPath);
+            command.Parameters.Add("toolsPath", toolsPath);
+            command.Parameters.Add("package", package);
+            pipeline.Commands.Add(command);
+
+            pipeline.Invoke();
+
+            runspace.Close();
+        }
+
+        public static void ExecuteInitIfExists(this IPackage package, String locationRoot)
+        {
+            package.ExecutePowershellScriptFileIfExists("init", locationRoot);
+        }
+
+        public static void ExecuteInstallIfExists(this IPackage package, String locationRoot)
+        {
+            package.ExecutePowershellScriptFileIfExists("install", locationRoot);
+        }
+
+        public static void ExecuteUninstallIfExists(this IPackage package, String locationRoot)
+        {
+            package.ExecutePowershellScriptFileIfExists("uninstall", locationRoot);
+        }
+    }
+}
