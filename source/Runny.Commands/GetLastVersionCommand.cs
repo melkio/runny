071c0a019491e463aa6b0fadf659dc6a16a2add6
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2013-07-09 13:06:49 +0200

    introduced command to retrive application's last available version

---
+++ source/Runny.Commands/GetLastVersionCommand.cs
@@ -0,0 +1,16 @@
+﻿using Agatha.Common;
+using System;
+
+namespace Runny.Commands
+{
+    public class GetLastVersionRequest : Request
+    {
+        public String ApplicationName { get; set; }
+        public String NugetSource { get; set; }
+    }
+
+    public class GetLastVersionResponse : Response
+    {
+        public String Version { get; set; }
+    }
+}
---
+++ source/Runny/Handlers/GetLastestVersionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Agatha.Common;
+using Agatha.ServiceLayer;
+using Common.Logging;
+using NuGet;
+using Runny.Commands;
+
+namespace Runny.Handlers
+{
+    class GetLastestVersionHandler : RequestHandler<GetLastVersionRequest, GetLastVersionResponse>
+    {
+        static ILog Log = LogManager.GetCurrentClassLogger();
+
+        public override Response Handle(GetLastVersionRequest request)
+        {
+            Log.Info(m => m("Handling request to retrieve last application version: {0}", request));
+
+            var response = CreateTypedResponse();
+
+            var source = PackageRepositoryFactory.Default.CreateRepository(request.NugetSource);
+
+            var sourcePackage = source.FindPackage(request.ApplicationName);
+            Log.Debug(m => m("Source package version: {0}", sourcePackage.Version));
+
+            response.Version = sourcePackage.Version.ToString();
+            return response;
+        }
+    }
+}
